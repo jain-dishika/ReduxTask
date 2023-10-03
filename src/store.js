01ceea6a254e4a8cdf7a26bd124104b4b6902582
aakash sharma <aakash@onplan.co>
2023-10-03 19:45:29 +0530

    updated

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-import { createStore } from "redux"
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./Reducer/Main";
 
-const  store  = createStore(rootReducer);
+const  store  = configureStore(rootReducer);
 export default store;
